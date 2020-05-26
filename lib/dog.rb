2552d0802352d7b5202131cf5bbda9d93dfa82af
Ian Hutson <ih00209@georgiasouthern.edu>
2020-05-26 16:55:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,16 +5,19 @@ class Dog
 def initialize(name)
 @@all << name
 end
+
 def self.all
 @@all
 end
+
 def self.clear_all
   @@all.clear
 end
+
 def self.print_all
   @@all.each do |name|
     puts "#{name}"
-end
+  end
 end
 
 end
